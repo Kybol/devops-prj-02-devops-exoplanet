918cdccf716681de100176afedcff6f7d2fd1a1e
Soxomer <omarjetti@gmail.com>
2022-12-19 16:00:59 +0100

    fix(eslint.rc): fix eslint jest conflict

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,7 +1,6 @@
-describe("Init CI test", () => {
-  test("return true", () => {
-    const result = true;
-    expect(result).toBe(true);
-  });
-});
-
+describe('Init CI test', () => {
+    test('return true', () => {
+        const result = true
+        expect(result).toBe(true)
+    })
+})
