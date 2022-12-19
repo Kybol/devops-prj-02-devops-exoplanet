3c2d262d1e611ec4d0da976517ee4ac0e8ffb80f
Soxomer <omarjetti@gmail.com>
2022-12-19 15:34:51 +0100

    test(test.js): added dummy test to check CI

---
+++ __tests__/test.js
@@ -0,0 +1,9 @@
+import {describe, expect, test} from '@jest/globals';
+
+describe("Init CI test", () => {
+  test("return true", () => {
+    const result = true;
+    expect(result).toBe(true);
+  });
+});
+
